f5f6e0cb13e6bf87ffa71a9075fe9dbb38a8f5a8
val868298-ai <val868298@gmail.com>
2026-06-15 03:08:30 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-print("STATUS:", response.status_code)
-print("TEXT:", response.text)
+from telegram import Update
+from telegram.ext import Application, MessageHandler, filters, ContextTypes
+import os
+
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+
+async def reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("бот работает 👍")
+
+app = Application.builder().token(TELEGRAM_TOKEN).build()
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, reply))
+
+app.run_polling()
